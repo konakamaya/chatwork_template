9c699d5597df76f772c38195ca93384612d9eb5d
Kanako Nishiyama <knshym@gmail.com>
2013-01-03 12:32:01 +0900

    activate jQuery

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("test");
+$(document).ready(function(){
+	//ボタンの子要素を追加(select boxとボタン)
+	$(".toolbar").append('<div class=\"cw_buttons cw_toggle template\"><select name=\"example\"><option value=\"サンプル1\">サンプル1</option><option value=\"サンプル2\">サンプル2</option><option value=\"サンプル3\">サンプル3</option></select><input type=\"button\" class=\"btn\" name=\"insert\" value=\"保存\"></button></div>');
+	//各ボタンを押した時の挙動(追加、削除)
+});
