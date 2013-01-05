1d69ef3eda26ac2d30e69b65d686fe4267bc694b
Kanako Nishiyama <knshym@gmail.com>
2013-01-05 13:43:52 +0900

    refactoring

--- script.js
+++ script.js
@@ -1,67 +1,55 @@
 $(document).ready(function(){
-	//ボタンの子要素を追加(select boxとボタン)
-	var templates = localStorage;
-	var selectbox = '<div class=\"cw_buttons cw_toggle template\"><select id=\"templates\" class=\"templates\" name=\"example\" > ';
-
-	for(var i = 0 ;i < templates.length; i++){
-		var key = templates.key(i);
-		var value = JSON.parse(templates.getItem(key));
-		selectbox += '<option value=\"'+key+'\">'+value.name+'</option> ';
-	}
-    selectbox += '</select><input id=\"insert\" type=\"button\" class=\"btn\" name=\"insert\" value=\"保存\"><input id=\"delete\" type=\"button\" class=\"btn\" name=\"delete\" value=\"削除\"><input id=\"use\" type=\"button\" class=\"btn\" name=\"use\" value=\"使用\"></div>';
+	// select boxとボタンを追加
+	var selectbox = '<div class=\"cw_buttons cw_toggle template\"> ';
+	selectbox += '<select id=\"templates\" class=\"templates\" name=\"example\" ></select>';
+    selectbox += '<input type=\"button\" id=\"insert\"  class=\"btn\" name=\"insert\" value=\"保存\">';
+	selectbox += '<input type=\"button\" id=\"delete\" class=\"btn\" name=\"delete\" value=\"削除\">';
+	selectbox += '<input type=\"button\" id=\"use\" class=\"btn\" name=\"use\" value=\"使用\">';
+	selectbox += '</div>';
 	$(".toolbar").append(selectbox);
 
-	//各ボタンを押した時の挙動(追加、削除)
+	selectBoxRebuild();
+
+	// 保存ボタンを押した時の挙動
 	$("#insert").click(function(){
-		var value = $("#cw_chattext").val();
-		if(value == "ここにメッセージ内容を入力"){
+		var message = $("#cw_chattext").val();
+		if(message == "ここにメッセージ内容を入力"){
 			alert("メッセージを入力して下さい。");
 			return;
 		}
 		var name = prompt("名前を入力して下さい。");
-		var uniqueId = new UniqueId();
-		var uid = uniqueId.create();
+
+		
 		if(name){
-			localStorage.setItem(uid, JSON.stringify({"name": name, "message": value}));
+			setMessage(name, message);
 		}else{
 			return;
 		}
 
-		$('#templates').empty();
-		var templates = localStorage;
-		for(var i = 0 ;i < templates.length; i++){
-			var key = templates.key(i);
-			var value = JSON.parse(templates.getItem(key));
-			var selectbox = '<option value=\"'+key+'\">'+value.name+'</option> ';
-			$('#templates').append(selectbox);
-		}		
+		selectBoxRebuild();
+
 	});
 
+	// 削除ボタンを押した時の挙動
 	$("#delete").click(function(){
 		var name = $("#templates :selected").text();
 		var key = $("#templates :selected").val();
 		var isConfirmed = confirm(name + "を削除しますか？");
+
 		if(isConfirmed){
 			localStorage.removeItem(key);
 		}else{
 			return;
 		}
 
-		$('#templates').empty();
-		var templates = localStorage;
-		for(var i = 0 ;i < templates.length; i++){
-			var key = templates.key(i);
-			var value = JSON.parse(templates.getItem(key));
-			var selectbox = '<option value=\"'+key+'\">'+value.name+'</option> ';
-			$('#templates').append(selectbox);
-		}
+		selectBoxRebuild();
 
 	});
 
+	// 使用ボタンを押した時の挙動
 	$('#use').click(function(){
 		var key = $("#templates :selected").val();
-		var value = JSON.parse(localStorage.getItem(key));
-		var message = value.message;
+		var message = getMessage(key);	
 		$('#cw_chattext').trigger("focus");
 		var oldMessage = '';
 		if($("#cw_chattext").val().length != 0){
@@ -69,8 +57,41 @@ $(document).ready(function(){
 		}
 		$("#cw_chattext").val(oldMessage + message);
 	});
+
+	//ローカルストレージにメッセージを登録する。
+	function setMessage(name, message){
+		// ユニークIDを生成する。
+		var uniqueId = new UniqueId();
+		var uid = uniqueId.create();
+
+		// localStorageにレコードを追加する。
+		localStorage.setItem(uid, JSON.stringify({"name": name, "message": message}));
+	}
+
+	//ローカルストレージからkeyを指定してメッセージを取得する。
+	function getMessage(key){
+		var value = localStorage.getItem(key);
+		var item = JSON.parse(value);
+		var message = item.message;
+		return message;
+	}
+
+
+	// ローカルストレージから登録済みの全てのメッセージを取得して
+	// optionタグを再構築するfunction
+	function selectBoxRebuild(){
+		$('#templates').empty();
+		var templates = localStorage;
+		for(var i = 0 ;i < templates.length; i++){
+			var key = templates.key(i);
+			var value = JSON.parse(templates.getItem(key));
+			var selectbox = '<option value=\"'+key+'\">'+value.name+'</option> ';
+			$('#templates').append(selectbox);
+		}
+	}
 });
 
+//  ユニークIDを生成するfunction
 var UniqueId = function() {};
 UniqueId.prototype = {
 	create: function() {
