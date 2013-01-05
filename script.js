ad289795809b9fc1eb9434a3682a2c3611458792
Kanako Nishiyama <knshym@gmail.com>
2013-01-05 21:14:22 +0900

    bug fix ボタンの位置が下にずれるバグを修正。

--- script.js
+++ script.js
@@ -1,103 +1,103 @@
 $(document).ready(function(){
-	// select boxとボタンを追加
-	var selectbox = '<div class=\"cw_buttons cw_toggle template\"> ';
-	selectbox += '<select id=\"templates\" class=\"templates\" name=\"example\" ></select>';
+    // select boxとボタンを追加
+    var selectbox = '<div class=\"cw_buttons cw_toggle template\"> ';
+    selectbox += '<select id=\"templates\" class=\"templates\" name=\"example\" ></select>';
     selectbox += '<input type=\"button\" id=\"insert\"  class=\"btn\" name=\"insert\" value=\"保存\">';
-	selectbox += '<input type=\"button\" id=\"delete\" class=\"btn\" name=\"delete\" value=\"削除\">';
-	selectbox += '<input type=\"button\" id=\"use\" class=\"btn\" name=\"use\" value=\"使用\">';
-	selectbox += '</div>';
-	$(".toolbar").append(selectbox);
+    selectbox += '<input type=\"button\" id=\"delete\" class=\"btn\" name=\"delete\" value=\"削除\">';
+    selectbox += '<input type=\"button\" id=\"use\" class=\"btn\" name=\"use\" value=\"使用\">';
+    selectbox += '</div>';
+    $(".toolbar").append(selectbox);
 
-	selectBoxRebuild();
+    selectBoxRebuild();
 
-	// 保存ボタンを押した時の挙動
-	$("#insert").click(function(){
-		var message = $("#cw_chattext").val();
-		if(message == "ここにメッセージ内容を入力"){
-			alert("メッセージを入力して下さい。");
-			return;
-		}
-		var name = prompt("名前を入力して下さい。");
+    // 保存ボタンを押した時の挙動
+    $("#insert").click(function(){
+        var message = $("#cw_chattext").val();
+        if(message == "ここにメッセージ内容を入力"){
+            alert("メッセージを入力して下さい。");
+            return;
+        }
+        var name = prompt("名前を入力して下さい。");
 
-		
-		if(name){
-			setMessage(name, message);
-		}else{
-			return;
-		}
+        
+        if(name){
+            setMessage(name, message);
+        }else{
+            return;
+        }
 
-		selectBoxRebuild();
+        selectBoxRebuild();
 
-	});
+    });
 
-	// 削除ボタンを押した時の挙動
-	$("#delete").click(function(){
-		var name = $("#templates :selected").text();
-		var key = $("#templates :selected").val();
-		var isConfirmed = confirm(name + "を削除しますか？");
+    // 削除ボタンを押した時の挙動
+    $("#delete").click(function(){
+        var name = $("#templates :selected").text();
+        var key = $("#templates :selected").val();
+        var isConfirmed = confirm(name + "を削除しますか？");
 
-		if(isConfirmed){
-			localStorage.removeItem(key);
-		}else{
-			return;
-		}
+        if(isConfirmed){
+            localStorage.removeItem(key);
+        }else{
+            return;
+        }
 
-		selectBoxRebuild();
+        selectBoxRebuild();
 
-	});
+    });
 
-	// 使用ボタンを押した時の挙動
-	$('#use').click(function(){
-		var key = $("#templates :selected").val();
-		var message = getMessage(key);	
-		$('#cw_chattext').trigger("focus");
-		var oldMessage = '';
-		if($("#cw_chattext").val().length != 0){
-			oldMessage = $("#cw_chattext").val() + "\n";
-		}
-		$("#cw_chattext").val(oldMessage + message);
-	});
+    // 使用ボタンを押した時の挙動
+    $('#use').click(function(){
+        var key = $("#templates :selected").val();
+        var message = getMessage(key);    
+        $('#cw_chattext').trigger("focus");
+        var oldMessage = '';
+        if($("#cw_chattext").val().length != 0){
+            oldMessage = $("#cw_chattext").val() + "\n";
+        }
+        $("#cw_chattext").val(oldMessage + message);
+    });
 
-	//ローカルストレージにメッセージを登録する。
-	function setMessage(name, message){
-		// ユニークIDを生成する。
-		var uniqueId = new UniqueId();
-		var uid = uniqueId.create();
+    //ローカルストレージにメッセージを登録する。
+    function setMessage(name, message){
+        // ユニークIDを生成する。
+        var uniqueId = new UniqueId();
+        var uid = uniqueId.create();
 
-		// localStorageにレコードを追加する。
-		localStorage.setItem(uid, JSON.stringify({"name": name, "message": message}));
-	}
+        // localStorageにレコードを追加する。
+        localStorage.setItem(uid, JSON.stringify({"name": name, "message": message}));
+    }
 
-	//ローカルストレージからkeyを指定してメッセージを取得する。
-	function getMessage(key){
-		var value = localStorage.getItem(key);
-		var item = JSON.parse(value);
-		var message = item.message;
-		return message;
-	}
+    //ローカルストレージからkeyを指定してメッセージを取得する。
+    function getMessage(key){
+        var value = localStorage.getItem(key);
+        var item = JSON.parse(value);
+        var message = item.message;
+        return message;
+    }
 
 
-	// ローカルストレージから登録済みの全てのメッセージを取得して
-	// optionタグを再構築するfunction
-	function selectBoxRebuild(){
-		$('#templates').empty();
-		var templates = localStorage;
-		for(var i = 0 ;i < templates.length; i++){
-			var key = templates.key(i);
-			var value = JSON.parse(templates.getItem(key));
-			var selectbox = '<option value=\"'+key+'\">'+value.name+'</option> ';
-			$('#templates').append(selectbox);
-		}
-	}
+    // ローカルストレージから登録済みの全てのメッセージを取得して
+    // optionタグを再構築するfunction
+    function selectBoxRebuild(){
+        $('#templates').empty();
+        var templates = localStorage;
+        for(var i = 0 ;i < templates.length; i++){
+            var key = templates.key(i);
+            var value = JSON.parse(templates.getItem(key));
+            var selectbox = '<option value=\"'+key+'\">'+value.name+'</option> ';
+            $('#templates').append(selectbox);
+        }
+    }
 });
 
 //  ユニークIDを生成するfunction
 var UniqueId = function() {};
 UniqueId.prototype = {
-	create: function() {
-		var randam = Math.floor(Math.random() * 1000);
-		var date = new Date();
-		var time = date.getTime();
-		return time.toString()  + '_' + randam;
-	}
+    create: function() {
+        var randam = Math.floor(Math.random() * 1000);
+        var date = new Date();
+        var time = date.getTime();
+        return time.toString()  + '_' + randam;
+    }
 };
